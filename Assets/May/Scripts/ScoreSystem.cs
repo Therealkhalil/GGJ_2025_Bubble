f80d44b78f95443677bd702d96501142eb24752e
Khalil Chabir <khalil.chabir96@gmail.com>
2025-01-26 13:55:50 +0100

    add UI and score ect

--- Assets/May/Scripts/ScoreSystem.cs
+++ Assets/May/Scripts/ScoreSystem.cs
@@ -1,14 +1,22 @@
 using System.Net.NetworkInformation;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using System.Collections;
+using System;
 
 public class ScoreSystem : MonoBehaviour
 {
+    public PlayerMovement players;
     public int Score = 0;
     GameObject ring;
     public bool isgoal = false;
     SphereCollider sphereColliderDonut;
     SphereCollider sphereColliderTrigger;
     [SerializeField] PickingSystem pickingSystem;
+    [SerializeField] TextMeshProUGUI scoreText;
+    [SerializeField] TextMeshProUGUI P1scoreText;
+    [SerializeField] TextMeshProUGUI P2scoreText;
 
 
     void Start()
@@ -16,7 +24,10 @@ public class ScoreSystem : MonoBehaviour
         ring = GameObject.FindGameObjectWithTag("Ring");
         sphereColliderDonut = ring.GetComponent<SphereCollider>();
         sphereColliderTrigger = gameObject.GetComponent<SphereCollider>();
-       
+
+        scoreText.gameObject.SetActive(false);
+
+
     }
 
     private void Update()
@@ -37,7 +48,21 @@ public class ScoreSystem : MonoBehaviour
         {
             Score++;
             Debug.Log("Current Score: " + Score);
+            StartCoroutine(GoalDisplaytxt());
             isgoal = true;
+            if (players.playerType == PlayerMovement.PlayerType.Player1)
+            {
+                P1scoreText.text = "Score : " + Score.ToString();
+            }
+            else
+            {
+                P2scoreText.text = "Score : " + Score.ToString();
+            }
         }
     }
+    private IEnumerator GoalDisplaytxt() {
+        scoreText.gameObject.SetActive(true);
+        yield return new WaitForSeconds(2);
+        scoreText.gameObject.SetActive(false);
+    }   
 }
